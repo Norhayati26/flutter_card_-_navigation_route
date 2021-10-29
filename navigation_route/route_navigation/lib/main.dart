c78b84cd57fdf5457d365db02b3dcbf3ab667d55
norhayati26 <ayaanorhayati26@gmail.com>
2021-10-30 02:35:07 +0800

    Flutter Card & Navigation Route

---
+++ navigation_route/route_navigation/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new HalamanPertama(),
+    routes: <String, WidgetBuilder>{
+      'HalamanPertama': (BuildContext context) => new HalamanPertama(),
+      'HalamanKeudua': (BuildContext context) => new HalamanKedua(),
+
+    }
+  ));
+}
+
+class HalamanPertama extends StatelessWidget {
+   @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Portal Menu Akses Route"),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: new IconButton(
+          icon: new Icon(
+            Icons.headset,
+            size: 50.0,
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, '/HalamanKedua');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class HalamanKedua extends StatelessWidget {
+   @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("SPEKER"),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: new IconButton(
+          icon: new Icon(
+            Icons.speaker,
+            size: 50.0,
+            color: Colors.red
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, '/HalamanPertama');
+          },
+        ),
+      ),
+    );
+  }
+}
